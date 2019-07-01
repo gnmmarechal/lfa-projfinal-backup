e8eea6430f631aa0d066b147dc5f96a78eea1a3b
Mario Alexandre Lopes Liberato <mliberato@gs2012.xyz>
2019-07-01 14:11:40 +0100

    mini update

--- Funções_finais/TestMain.java
+++ Funções_finais/TestMain.java
@@ -1,8 +1,13 @@
+import java.util.*;
 public class TestMain {
 	
 	public static void main (String[] args) {
 		Plugin cropPlugin = new CropPlugin();
-		System.out.println(cropPlugin.getFunction());
+		System.out.println("Function Name: " + cropPlugin.getFunctionName());
+		System.out.println("Argument Count: " + cropPlugin.getFunctionArgumentCount());
+		System.out.println("Arguments: " + Arrays.toString(cropPlugin.getFunctionArguments()));
+		System.out.println("Dependencies: " + Arrays.toString(cropPlugin.getFunctionDependencies()));
+		System.out.println("Function Body:\n" + cropPlugin.getFunction());
 	}
 }
 
