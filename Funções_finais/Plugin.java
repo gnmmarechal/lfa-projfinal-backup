1432524c38980cd11842e75d65a709f1a512ab62
Mario Alexandre Lopes Liberato <mliberato@gs2012.xyz>
2019-07-01 16:03:27 +0100

    adicionada mais uma função ao plugin

--- Funções_finais/Plugin.java
+++ Funções_finais/Plugin.java
@@ -3,6 +3,7 @@ public interface Plugin
 	public String getFunctionName();
 	public int getFunctionArgumentCount();
 	public String[] getFunctionArguments();
+	public String[] getPluginDependencies();
 	public String[] getFunctionDependencies();
 	public String getFunction();
 }
