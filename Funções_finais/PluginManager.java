f4f745abb04529bf1bda57648843169f5ad4094f
Mario Alexandre Lopes Liberato <mliberato@gs2012.xyz>
2019-07-07 17:55:04 +0100

    Limpeza parcial

--- Funções_finais/PluginManager.java
+++
@@ -1,6 +0,0 @@
-public abstract class PluginManager {
-	public static Plugin load(String name) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		Class<?> c = Class.forName(name);	
-		return (Plugin) c.newInstance();
-	}
-}
